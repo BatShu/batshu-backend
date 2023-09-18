cd740474934d7cb4216fac74cac605aab1940c8f
bik1111 <bik1111@khu.ac.kr>
2023-09-18 11:37:40 +0900

    fix : main.yaml

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
 FROM node:16-alpine
 
+# ⭐ 'ARG' 예약어를 통해 인자로 전달 받아야 한다.
+ARG PROJECT_ID \
+    PRIVATE_KEY_ID \
+    CLIENT_EMAIL
+
+
+# ⭐ 'ENV' 예약어를 통해 전달받은 값을 실제 값과 매칭시켜야 한다.
+ENV PRIVATE_KEY_ID=${PRIVATE_KEY_ID} \
+    PRIVATE_KEY_ID=${PRIVATE_KEY_ID} \
+    CLIENT_EMAIL=${CLIENT_EMAIL} 
+
+
 WORKDIR /src
 
 COPY package*.json ./
--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -1,6 +1,19 @@
 import * as admin from 'firebase-admin';
+import dotenv from "dotenv";
+dotenv.config();
 
-admin.initializeApp();
+let privateKey: string | undefined = process.env.FIREBASE_PRIVATE_KEY;
 
-export const administrator = admin.initializeApp();
+if (privateKey) {
+  privateKey = privateKey.replace(/\\n/g, '\n');
+}
+
+
+export const administrator = admin.initializeApp({
+  credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID, 
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,  
+      privateKey: privateKey
+  }),
+});
 
