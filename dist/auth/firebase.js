cf2aed18350cc1af4454f32e74d3ca972103ee34
bik1111 <bik1111@khu.ac.kr>
2023-09-22 15:07:40 +0900

    fix : main yaml

--- dist/auth/firebase.js
+++ dist/auth/firebase.js
@@ -25,19 +25,25 @@ var __importStar = (this && this.__importStar) || function (mod) {
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.administrator = void 0;
 var admin = __importStar(require("firebase-admin"));
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-var _a = process.env, PROJECT_ID = _a.PROJECT_ID, CLIENT_EMAIL = _a.CLIENT_EMAIL, PRIVATE_KEY = _a.PRIVATE_KEY;
+var _b = {
+    PROJECT_ID: process.env.PROJECT_ID,
+    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+    PRIVATE_KEY: (_a = process.env.PRIVATE_KEY) === null || _a === void 0 ? void 0 : _a.replace(/\\n/g, '\n'), // 개행 문자 처리
+}, PROJECT_ID = _b.PROJECT_ID, CLIENT_EMAIL = _b.CLIENT_EMAIL, PRIVATE_KEY = _b.PRIVATE_KEY;
 if (!PRIVATE_KEY) {
     throw new Error('PRIVATE_KEY is missing in the environment variables');
 }
+console.log(PRIVATE_KEY);
 exports.administrator = admin.initializeApp({
     credential: admin.credential.cert({
         projectId: PROJECT_ID,
         clientEmail: CLIENT_EMAIL,
-        privateKey: PRIVATE_KEY.replace(/\\n/g, '\n')
+        privateKey: PRIVATE_KEY
     }),
 });
--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -2,17 +2,24 @@ import * as admin from 'firebase-admin';
 import dotenv from "dotenv";
 dotenv.config(); 
 
-const { PROJECT_ID, CLIENT_EMAIL, PRIVATE_KEY } = process.env;
+const { PROJECT_ID, CLIENT_EMAIL, PRIVATE_KEY } = {
+  PROJECT_ID: process.env.PROJECT_ID,
+  CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+  PRIVATE_KEY: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'), // 개행 문자 처리
+};
+
+
 
 if (!PRIVATE_KEY) {
   throw new Error('PRIVATE_KEY is missing in the environment variables');
 }
 
+console.log(PRIVATE_KEY);
 
 export const administrator = admin.initializeApp({
   credential: admin.credential.cert({
       projectId: PROJECT_ID,
       clientEmail: CLIENT_EMAIL,  
-      privateKey: PRIVATE_KEY.replace(/\\n/g, '\n')
+      privateKey: PRIVATE_KEY
   }),
 });
