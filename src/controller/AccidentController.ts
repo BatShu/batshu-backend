52392b4cbe4ff848eac1522e726fdf181dcddbe8
jeongharam <04harams77@gmail.com>
2023-09-26 19:16:44 +0900

    edit code

--- src/controller/AccidentController.ts
+++ src/controller/AccidentController.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
 
 const accidentService = require("../service/AccidentService");
-const auth = require("../auth/auth");
-const admin = require('firebase-admin');
 const crypto = require('crypto');
 const sharp = require('sharp')
 
@@ -39,13 +37,6 @@ declare global {
     size: number;
   }
 
-  interface s3ClientParams {
-    credentials: {
-      accessKeyId: string;
-      secretAccessKey: string;
-    };
-    region: string;
-  }
 }
 
 dotenv.config();
@@ -62,7 +53,7 @@ AWS.config.update({
   region: bucketRegion,                      // 사용하는 AWS 지역을 여기에 입력
 });
 
-const s3params:s3ClientParams = {
+const s3params = {
   credentials: {
     accessKeyId: accessKey,
     secretAccessKey: secretAccessKey
@@ -76,19 +67,15 @@ const s3 = new S3Client(s3params);
 // Header - 
 // key  : Authorization
 // value : Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjE5MGFkMTE4YTk0MGFkYzlmMmY1Mzc2YjM1MjkyZmVkZThjMmQwZWUiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoi7KCV7ZWY656MIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FBY0hUdGNXLUJOZ21qOWV0N0J5UUlzYjNfLVJKUnFQX3dQaFZKTmRTZGNpWXNnVj1zOTYtYyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9zeW5lcmd5LXRlc3QtYTFmMjQiLCJhdWQiOiJzeW5lcmd5LXRlc3QtYTFmMjQiLCJhdXRoX3RpbWUiOjE2OTQ4ODQyMzQsInVzZXJfaWQiOiJGWG55SlozcWw2UzJoaVpGRG5NaGNRckZSNWcyIiwic3ViIjoiRlhueUpaM3FsNlMyaGlaRkRuTWhjUXJGUjVnMiIsImlhdCI6MTY5NDg4NDIzNCwiZXhwIjoxNjk0ODg3ODM0LCJlbWFpbCI6IjA0aGFyYW1zNzdAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZ29vZ2xlLmNvbSI6WyIxMTU5MDQzMjk4NzY5MzQ1MTQ1NzYiXSwiZW1haWwiOlsiMDRoYXJhbXM3N0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.cmcO6RKWQMJaD4pUruiQ8ofYo-DT11n86om0R0W80crdnAragSR-hARBJ7FoQuuieCHokRnuNkVAHRrSxDjm1DuCpnKgHXcOleA82QSUcjY2BSvAQBkGsqACR6Vp6XDXRpbDnsBG3tpgu0TS76EJUzcWTIVkTLZJnH4Gyn4-onD2L8yiyqVWj6U2IIYxzrAhcIWA7Dejw7cJltouwwMVRYpvIVnBKHLd8hs64RihLgOxtaZAD5T8fsn5eyDyBjcRWRZ6lBPSOfqbENVUPJGNUY0buFqbad1auPbCSieGuSp3XXxMDyiWKoutWY3jWyJ0Qgy9llxPjIG7cXwTAAm6wg
-// body -
+// body - multi form-data
 // {
 //   "contentTitle" : "아..큰일남",
 //   "contentDescription"  : "뺑소니당했어",
-//   "pictureUrl" : [
-//       "https://www.socialfocus.co.kr/news/photo/202101/9416_15430_221.jpg",
-//       "https://img.freepik.com/free-photo/image-of-a-auto-accident-involving-two-cars_613910-7924.jpg?w=2000",
-//       "https://blog.kakaocdn.net/dn/5jKyK/btq6GpHQ8yZ/kQfKk9eRuGtzmLzpKavkTk/img.png"
-//   ],
 //   "accidentTime" : [
 //     "2023-11-02T00:05:04.123",
 //     "2023-11-02T00:06:04.123"
 //   ],
+//   "pictureUrl" : -> 이미지 파일들 select
 //   "accidentLocation" : {
 //        "x" : "32.234234234",
 //        "y" : "152.234234234"
@@ -111,14 +98,13 @@ export const postAccident = async (req: CustomRequest, res: Response) => {
         for (let img of images){
           const fileName:string = crypto.randomBytes(16).toString('hex');
           const buffer = await sharp(img.buffer).resize({height: 1920, width: 1080, fit: "contain"}).toBuffer()
-          console.log(img);
           const params = {
             Bucket: bucketName,
             Key: `${fileName}.${img.originalname.split('.').pop()}`,
             Body: buffer,
             ContentType: img.mimetype
           }
-          
+
           pictureUrl.push(`https://${params.Bucket}.s3.amazonaws.com/${params.Key}`);
 
           const command = new PutObjectCommand(params);
@@ -137,8 +123,6 @@ export const postAccident = async (req: CustomRequest, res: Response) => {
           uid : uid,
           bounty : req.body.bounty
         }
-
-        // console.log(passedData);
 
         const resData:ApiResponse = await accidentService.createAccident(passedData);
         res.status(200).json(resData);
