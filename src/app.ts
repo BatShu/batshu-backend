88e4428f348ae22c9f5d2d253673fcfd893d5526
bluejoyq <bluejoy32@gmail.com>
2023-10-16 02:40:06 +0900

    feat: eslint 추가

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,32 @@
-import dotenv from 'dotenv'
+/* eslint-disable import/first */
 import path from 'path';
-const envPath = process.env.NODE_ENV === "production" ? "../.env.production" : "../.env.development"
-dotenv.config({
-  debug: true,
-  path: path.resolve(__dirname, envPath)
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({
+  path: path.resolve(__dirname, process.env.NODE_ENV === 'production' ? '../.env.production' : '../.env.development')
 });
-import express, {Express, Request, Response } from "express";
+import express, { Express, type Request, type Response } from 'express';
 import bodyParser from 'body-parser';
-import AccidentRouter from "./routers/AccidentRouter";
-import ObserveRouter from "./routers/ObserveRouter";
-import UserRouter from "./routers/UserRouter";
-
-
-
-
+import AccidentRouter from './routers/AccidentRouter';
+import ObserveRouter from './routers/ObserveRouter';
+import UserRouter from './routers/UserRouter';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());   
+app.use(bodyParser.json());
 
+app.use('/api/user', UserRouter);
+app.use('/api/accident', AccidentRouter);
+app.use('/api/observe', ObserveRouter);
 
-app.use("/api/user", UserRouter);
-app.use("/api/accident", AccidentRouter);
-app.use("/api/observe", ObserveRouter);
-
-app.get("/hello", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/hello', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
-const handleListening = () =>
-  console.log(`✅Server listenting on http://localhost:${PORT} 🚀 `);
+const handleListening = () => { console.log(`✅Server listenting on http://localhost:${PORT} 🚀 `); };
 
 app.listen(PORT, handleListening);
-
-
 
 export default app;
