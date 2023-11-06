e090baf925ca3e48e4d2ece22725145b93689f4d
haramjeong <04harams77@gmail.com>
2023-11-06 16:20:58 +0900

    feat : 채팅 파일 전송 기능 추가

--- src/service/MessageService.ts
+++ src/service/MessageService.ts
@@ -1,17 +1,87 @@
 import { type ApiResponse } from 'src/domain/response';
-import { type SelectRoomRow, type SelectMessageRow, type SendMessageRequest, type ReadChatData, type Chat } from '../interface/chat';
+import { type SelectRoomRow, type SelectMessageRow, type SendMessageRequest, type ReadChatData, type Chat, type SendFileRequest } from '../interface/chat';
 import { type PoolConnection } from 'mysql2/promise';
 import { insertMessageRow, selectMessageRow, selectRoomRow } from '../Repository/MessageRepository';
 import pool from '../config/database';
+import crypto from 'crypto';
+import AWS from 'aws-sdk';
+import { PutObjectCommand, S3Client, type S3ClientConfig } from '@aws-sdk/client-s3';
+
+const bucketName: string = process.env.BUCKET_NAME_HARAM ?? '';
+const accessKey: string = process.env.ACCESS_KEY_HARAM ?? '';
+const secretAccessKey: string = process.env.SECRET_ACCESS_KEY_HARAM ?? '';
+
+AWS.config.update({
+  accessKeyId: accessKey, // AWS Access Key ID를 여기에 입력
+  secretAccessKey, // AWS Secret Access Key를 여기에 입력
+  region: 'ap-northeast-2' // 사용하는 AWS 지역을 여기에 입력
+});
+
+const s3params: S3ClientConfig = {
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey
+  },
+  region: 'ap-northeast-2'
+};
+
+const s3 = new S3Client(s3params);
 
 export const insertMessage = async (messageObject: SendMessageRequest): Promise<ApiResponse> => {
   try {
     const connection: PoolConnection = await pool.getConnection();
     const success: boolean = await insertMessageRow(connection, messageObject);
+    connection.release();
     if (success) {
       const answer: ApiResponse = {
         ok: true,
         msg: 'successfully regist chat'
+      };
+      return answer;
+    } else {
+      throw new Error('Insertion failed'); // Throw an error if insertMessageRow returns false
+    }
+  } catch (err) {
+    console.log(err);
+    const answer: ApiResponse = {
+      ok: false,
+      msg: 'regist fail'
+    };
+    return answer;
+  }
+};
+
+export const insertFile = async (fileObject: SendFileRequest): Promise<ApiResponse> => {
+  try {
+    const fileName: string = crypto.randomBytes(16).toString('hex');
+
+    const buffer: Buffer = Buffer.from(fileObject.file.fileData);
+
+    const params = {
+      Bucket: bucketName,
+      Key: `${fileName}.${fileObject.file.filename.split('.').pop()}`,
+      Body: buffer
+    };
+
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+
+    const fileUrl: string = `https://${params.Bucket}.s3.amazonaws.com/${params.Key}`;
+
+    const passedData: SendMessageRequest = {
+      roomId: fileObject.roomId,
+      sendUserUid: fileObject.sendUserUid,
+      message: fileUrl
+    };
+
+    const connection: PoolConnection = await pool.getConnection();
+    const success: boolean = await insertMessageRow(connection, passedData);
+    connection.release();
+
+    if (success) {
+      const answer: ApiResponse = {
+        ok: true,
+        msg: 'successfully regist file'
       };
       return answer;
     } else {
