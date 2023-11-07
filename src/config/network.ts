9f0dd88e29d5568557f3ba1c813788f970720be8
bluejoyq <bluejoy32@gmail.com>
2023-11-07 22:10:29 +0900

    feat: add origin

--- src/config/network.ts
+++ src/config/network.ts
@@ -1,6 +1,6 @@
 import type { CorsOptions, CorsOptionsDelegate } from 'cors';
 
 export const corsOption: CorsOptions | CorsOptionsDelegate = {
-  origin: process.env.CLIENT_BASE_URL ?? 'http://localhost:5173',
+  origin: ['https://batshu-c203f.firebaseapp.com', 'https://batshu-c203f.web.app', 'http://localhost:5173'],
   credentials: true
 };
